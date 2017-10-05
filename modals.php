a9820aa8ba7206d94bf036a2d657882daf0de93a
Darkelf151 <alan@nxav.co.uk>
2017-10-05 17:06:09 +0100

    woriking on the client modal

--- modals.php
+++ modals.php
@@ -19,7 +19,7 @@
 
 <!-- About modal -->
   <div class="modal about-modal fade" id="myModal" class="clientBooking" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header"> 
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>            
